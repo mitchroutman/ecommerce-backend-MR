f3fd2f2fc4be8166f1adfae50570a7652edf940b
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-24 16:07:30 -0500

    attempt 1 at delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,8 +21,19 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+	await Category.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+	.then((deletedCategory) => {
+		res.json(`Category removed.`);
+	})
+	.catch((err) => {
+		res.json(err);
+	});
 });
 
 module.exports = router;
