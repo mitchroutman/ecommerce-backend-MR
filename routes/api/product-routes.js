bc73fddfbdbc4e2546af7b1e67f97a107e2e8d2c
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-25 16:12:45 -0500

    WIP product routes - find all working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+  .then(allProd => res.json(allProd))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, Tag]
+  })
+  .then(singleProd => res.json(singleProd))
 });
 
 // create new product
